2e2555e0aac74100756ee2ffb87bd62836d18937
Jimmers2001 <38543433+Jimmers2001@users.noreply.github.com>
2023-05-01 16:26:21 -0400

    on_message function works with commands

--- bot.py
+++ bot.py
@@ -6,17 +6,35 @@ import os
 from dotenv import load_dotenv, find_dotenv
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHANNEL_ID = os.getenv("CHANNEL_ID")
+BOT_TOKEN = os.getenv("BOT_TOKEN") #must be string
+CHANNEL_ID = int(os.getenv("CHANNEL_ID")) #must be integer
 
 #initialize bot that intents to use all discord features
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
+#####################################################################################
+#                                    EVENTS                                          #
+#####################################################################################
 @bot.event
 async def on_ready(): #runs when the bot is initialized
     channel = bot.get_channel(CHANNEL_ID)
-    await channel.send("Hey channel, I'm JimBot and I'm cool!") #blocking await call
+    await channel.send("Hey channel, I'm JimBot and I'm cool!")
 
+@bot.event
+async def on_message(message):
+    
+    #if the message is from the bot itself, do nothing
+    if (message.author == bot.user):
+        return
+
+    #MUST HAVE PROCESS_COMMANDS so other commands can be done as well
+    await bot.process_commands(message)
+
+    
+
+#####################################################################################
+#                                    COMMANDS                                       #
+#####################################################################################
 @bot.command()
 async def hello(ctx): #argv
     await ctx.send("Hiya")
@@ -27,6 +45,7 @@ async def add(ctx, *arr): #argv
     for i in arr:
         result += int(i)
     await ctx.send(f"Result = {result}")
+
 
 #loop and continuously run until the bot is ended
 bot.run(BOT_TOKEN)
